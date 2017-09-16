717875d96350e2cc5499e0cb14d2b850fff399e2
Jyoti Rani <ranijyoti392@gmail.com>
2017-09-16 20:23:20 +0530

    Added find Grade Problem

--- src/Conditionals.kt
+++ src/Conditionals.kt
@@ -1,3 +1,23 @@
 /**
  * Created by jyotirani on 16/09/17.
  */
+fun main(args: Array<String>) {
+
+    print("Enter age: ")
+    val age = readLine()!!.toInt()
+    if (age > 0) {
+        if (age < 5) {
+            println("Go to Pre School.")
+        } else if (age == 5) {
+            println("Go to Kindegarten.")
+        } else if (age > 5 && age <=17) {
+            println("Go to grade ${age - 5}")
+        } else {
+            println("Go to College.")
+        }
+    } else {
+        println("Enter correct age.")
+    }
+
+
+}
