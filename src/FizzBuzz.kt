54b589b0e88992669837b7e1e314d6f658ff896e
Jyoti Rani <ranijyoti392@gmail.com>
2017-09-16 21:18:21 +0530

    Number iterator FizzBuzz Problem

---
+++ src/FizzBuzz.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by jyotirani on 16/09/17.
+ */
+fun main(args: Array<String>) {
+    print("Enter number: ")
+    val number = readLine()!!.toInt()
+    for (output in 1..number) {
+        if (output % 3 == 0 && output % 5 == 0) {
+            println("FizzBuzz")
+        } else if (output % 5 == 0) {
+            println("Buzz")
+        } else if (output % 3 == 0) {
+            println("Fizz")
+        } else {
+            println(output)
+        }
+    }
+}
